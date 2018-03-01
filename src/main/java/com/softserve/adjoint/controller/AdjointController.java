59b66a6c2a9b43d8f4fca59f8bab9d67d377ae16
Oleksii Bilogurov <lehaalexx@gmail.com>
2018-03-01 22:58:20 +0100

    docker

--- src/main/java/com/softserve/adjoint/controller/AdjointController.java
+++ src/main/java/com/softserve/adjoint/controller/AdjointController.java
@@ -1,21 +1,40 @@
 package com.softserve.adjoint.controller;
 
 import adjoint.uplink_sdk.client.Response;
-import com.softserve.adjoint.service.AdjointService;
-import org.springframework.beans.factory.annotation.Autowired;
+import adjoint.uplink_sdk.client.UplinkSDK;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rest/v1/")
 public class AdjointController {
 
-    @Autowired
-    private AdjointService adjointService;
+//    @Autowired
+//    private AdjointService adjointService;
+
+    private String unsecure = "http://";
+    private UplinkSDK uplink = new UplinkSDK(unsecure, "localhost", "8545");
 
     @RequestMapping(value = "/blocks", method = RequestMethod.GET)
-    public Response getBLocks() {
-        return adjointService.getBlocks();
+    public Response getBlocks() {
+        return uplink.getBlocks();
     }
+
+    @RequestMapping(value = "/accounts", method = RequestMethod.GET)
+    public Response getAccounts() {
+        return uplink.getAccounts();
+    }
+
+    @RequestMapping(value = "/account", method = RequestMethod.GET)
+    public Response getAccount(@RequestParam String address) {
+        return uplink.getAccount(address);
+    }
+
+    @RequestMapping(value = "/assets", method = RequestMethod.GET)
+    public Response getAssets() {
+        return uplink.getAssets();
+    }
+
 }
